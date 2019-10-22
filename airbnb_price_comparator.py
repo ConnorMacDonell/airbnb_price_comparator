d3caa0cdc4c742fafba512d3cc0cc6f3f261e755
Connor MacDonell <connor.macdonell1@gmail.com>
2019-10-22 17:04:16 +0900

    Reworked methodology for dealing with different site loads. Added price scrape.

--- airbnb_price_comparator.py
+++ airbnb_price_comparator.py
@@ -8,43 +8,50 @@ from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 
 
+def get_prices(attr_class, price_class, soup):
+    for listing in soup.find_all('div', class_="_8ssblpx"):
+        if "1 bedroom" in listing.find('div', class_=attr_class).text:
+            for price in listing.find_all('span', class_=price_class):
+                print(price.text)
+
+
 url = "https://www.airbnb.com/s/saigon/homes?source=mc_search_bar&search_type=section_navigation&screen_size=large&checkin=2019-10-31&checkout=2019-11-07&adults=1&refinement_paths%5B%5D=%2Fhomes&place_id=ChIJ0T2NLikpdTERKxE8d61aX_E"
 driver = webdriver.Firefox()
 driver.implicitly_wait(30)
 driver.get(url)
-delay = 5
+delay = 1
 
 first = True
 second = False
 
-first_class = "_1s7voim"
-second_class = "_1jlnvra2"
-curr_class = ""
+first_attr_class = "_1s7voim"
+first_price_class = "_1p7iugi"
+
+second_attr_class = "_1jlnvra2"
+second_price_class = "_61b3pa"
 
 try:
     listing_details = WebDriverWait(driver, delay).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'div._1s7voim')))
+        EC.presence_of_element_located((By.CSS_SELECTOR, f"div.{first_attr_class}")))
 except TimeoutException:
     try:
         first = False
         second = True
         listing_details = WebDriverWait(driver, delay).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "div._1jlnvra2")))
+            EC.presence_of_element_located((By.CSS_SELECTOR, f"div.{second_attr_class}")))
     except TimeoutException:
         second = False
         print("Loading Timeout")
 
+driver.quit()
+
 soup = BeautifulSoup(driver.page_source, "lxml")
 
 if first:
-    curr_class = first_class
+    print("Type 1")
+    print()
+    get_prices(first_attr_class, first_price_class, soup)
 elif second:
-    curr_class = second_class
-
-
-for listing in soup.find_all('div', class_=curr_class):
-    if "1 bedroom" in listing.text:
-        print(listing.text)
-
-
-driver.quit()
+    print("Type 2")
+    print()
+    get_prices(second_attr_class, second_price_class, soup)
